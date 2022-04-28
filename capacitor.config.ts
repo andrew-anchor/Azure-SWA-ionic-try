429b0cdf7586a33b3c58dfd9c9be48217e5d6a6c
andrew-anchor <andrew.wu@anchor-meta.com>
2022-04-28 18:34:39 +0800

    feat: ionic list init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: ' azure-swa-ionic-try',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
